785c994403233610ed6fa9b169d6547170af1a23
JdM <julien@rmbi.ch>
2023-06-11 16:22:18 +0200

    consoleSize function

--- main.go
+++ main.go
@@ -78,3 +78,28 @@ func convertToRGBA(img image.Image) *image.RGBA {
 
 	return rgba
 }
+
+
+func consoleSize() (int, int) {
+	cmd := exec.Command("stty", "size")
+	cmd.Stdin = os.Stdin
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := string(out)
+	s = strings.TrimSpace(s)
+	sArr := strings.Split(s, " ")
+
+	heigth, err := strconv.Atoi(sArr[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	width, err := strconv.Atoi(sArr[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	return heigth, width
+}
