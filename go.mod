c2b1f1afff63cbb93cdfeadb7b0155bbc6d32a1d
JdM <julien@rmbi.ch>
2023-06-11 14:16:43 +0200

    get pixels value

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/judemont/Image_Displayer
 
 go 1.20
+
+require github.com/h2non/bimg v1.1.9 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
+package main
+
 import (
 	"fmt"
+	"image"
+	_ "image/jpeg"
+	"log"
+	"os"
 )
 
-func main(){
-	
+func main() {
+	imageFile, err := os.Open("test_image.jpeg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer imageFile.Close()
+
+	imgData, _, err := image.Decode(imageFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rgba := convertToRGBA(imgData)
+	fmt.Println(rgba.At(0, 0)) // Example: accessing the color at pixel (0, 0)
+}
+
+func convertToRGBA(img image.Image) *image.RGBA {
+	bounds := img.Bounds()
+	rgba := image.NewRGBA(bounds)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			rgba.Set(x, y, img.At(x, y))
+		}
+	}
+
+	return rgba
 }
